585712950810de5faa355cd95a4de11e628b2cdf
duy1111 <maixuanduy0605@gmail.com>
2023-07-16 12:16:41 +0700

    Initial commit#4

--- src/components/Images/Images.js
+++ src/components/Images/Images.js
@@ -3,7 +3,6 @@ import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ImageCards";
 import Particle from "../Particle";
 
-import editor from "../../Assets/Projects/codeEditor.png";
 
 
 function Projects() {
