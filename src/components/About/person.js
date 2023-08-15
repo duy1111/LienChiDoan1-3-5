53c2c3b03f9f61da41e56f0bd5125a6745119840
duy1111 <maixuanduy0605@gmail.com>
2023-08-15 15:07:00 +0700

    update detail user#7

--- src/components/About/person.js
+++ src/components/About/person.js
@@ -1,35 +1,38 @@
 import React from "react";
 import { Button, Col, Row } from "react-bootstrap";
 import { user } from "../../common/data/user";
-import {AiOutlineUser} from "react-icons/ai"
-import { useNavigate } from 'react-router-dom';
+import { AiOutlineUser } from "react-icons/ai";
+import { useNavigate } from "react-router-dom";
 function Person() {
   const navigate = useNavigate();
 
   let changeDetail = (id) => {
-    navigate(`/user/${id}`)
-  }
+    navigate(`/user/${id}`);
+  };
+  
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      {user?.length > 0 && user.map((item,index) => {
-        if(item.role === 'R1'){
-          return (
-          
-            <Col xs={12} md={3} sm={6} xl={2}  className="tech-icons">
-              <img className="image" width={150} height={150} src={item?.image} alt={item?.title}  />
-              <p>{item.name}</p>
-              <Button variant="primary" onClick={() => changeDetail(item.id)}>
-                <AiOutlineUser /> &nbsp;
+      {user?.length > 0 &&
+        user.map((item, index) => {
+          if (item.role === "R1") {
+            return (
+              <Col key={index} xs={12} md={3} sm={6} xl={2} className="tech-icons">
+                <img
+                  className="image"
+                  width={150}
+                  height={150}
+                  src={item?.image}
+                  alt={item?.title}
+                />
+                <p>{item.name}</p>
+                <Button variant="primary" onClick={() => changeDetail(item.id)}>
+                  <AiOutlineUser /> &nbsp;
                   {"Info"}
                 </Button>
-            </Col>
-          )
-        }
-        
-      })}
-      
-      
-      
+              </Col>
+            );
+          }
+        })}
     </Row>
   );
 }
--- src/components/About/person2.js
+++ src/components/About/person2.js
@@ -1,32 +1,52 @@
 import React from "react";
 import { Button, Col, Row } from "react-bootstrap";
 import { user } from "../../common/data/user";
-import {AiOutlineUser} from "react-icons/ai"
+import { AiOutlineUser } from "react-icons/ai"
 import { useNavigate } from 'react-router-dom';
 
 function Person2() {
- const navigate = useNavigate();
+  const navigate = useNavigate();
 
   let changeDetail = (id) => {
     navigate(`/user/${id}`)
   }
+
+  function extractLastName(name) {
+    const parts = name.split(' ');
+    return parts[parts.length - 1];
+  }
+  let data = user.filter((item) => item.role === "R2");
+  data.sort((a, b) => {
+    const lastNameA = extractLastName(a.name.trim()).toUpperCase();
+    const lastNameB = extractLastName(b.name.trim()).toUpperCase();
+
+    if (lastNameA < lastNameB) {
+      return -1;
+    }
+    if (lastNameA > lastNameB) {
+      return 1;
+    }
+    return 0;
+  });
+
+  console.log(data);
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      {user?.length > 0 && user.map((item,index) => {
-        if(item.role === 'R2'){
-          return (
-          
-            <Col xs={12} md={3} sm={6} xl={2} className="tech-icons">
-              <img className="image" width={150} height={150} src={item?.image} alt={item?.title}  />
-              <p>{item.name}</p>
-              <Button variant="primary" onClick={() => changeDetail(item.id)}>
-                <AiOutlineUser /> &nbsp;
-                  {"Info"}
-                </Button>
-            </Col>
-          )
-        }
-        
+      {data?.length > 0 && data.map((item, index) => {
+
+        return (
+
+          <Col xs={12} md={3} sm={6} xl={2} className="tech-icons">
+            <img className="image" width={150} height={150} src={item?.image} alt={item?.title} />
+            <p>{item.name}</p>
+            <Button variant="primary" onClick={() => changeDetail(item.id)}>
+              <AiOutlineUser /> &nbsp;
+              {"Info"}
+            </Button>
+          </Col>
+        )
+
+
       })}
     </Row>
   );
