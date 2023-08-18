111f4ef8b2db1d61d9ad2f2fa8d947969997636e
duy1111 <maixuanduy0605@gmail.com>
2023-08-19 03:27:06 +0700

    update4

--- src/components/IntroVideo/IntroVideo.js
+++ src/components/IntroVideo/IntroVideo.js
@@ -1,8 +1,16 @@
 import React from 'react'
-
 const IntroVideo = () => {
+  let props = '4Erhrcz0I-4'
   return (
-    <div style={{height:"880px",color:'red',display:"flex",justifyContent:"center",alignItems:"center"}} >IntroVideo</div>
+    <div style={{height:"880px",color:'red',display:"flex",justifyContent:"center",alignItems:"center"}} >
+       <iframe
+        width="1000px"
+        height="600px"
+        src={`https://www.youtube.com/embed/${props}`}
+        frameborder="0"
+        allowfullscreen
+      ></iframe>
+    </div>
   )
 }
 
